f0e552fc1909d809ddab7b9010d8d8b2665f9706
f1214638 <f1214638@csdlabad.local>
2022-10-27 15:34:22 +0800

    27/10 Client class structure

--- src/Client.java
+++ src/Client.java
@@ -1,76 +1,81 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
-    final String ID; // the ID of the user
-    public String username; // the username of the user
-    private String password; // the password of the user
+    public static String username; // the username of the user
+    private static String password; // the password of the user
     public boolean status; // the online/offline status of the user
+    private static boolean login_success = false;
     Socket socket;
     DataInputStream in;
     DataOutputStream out;
-    Scanner scanner = new Scanner(System.in);
     byte[] buffer = new byte[1024];
 
+    public Client(String serverIP, int port) throws IOException {
+        System.out.printf("Connecting to %s:%d\n", serverIP, port);
+        Socket socket = new Socket(serverIP, port);
+        DataInputStream in = new DataInputStream(socket.getInputStream());
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        byte[] buffer = new byte[1024];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1)Registration\n2)Login");
+        int option = sc.nextInt(); // choose register or login
+        sc.nextLine();
+        if (option == 1) { // Registration
+            System.out.println("=== Registration ===");
+            // input the username and password
+            System.out.println("Input username:");
+            username = sc.nextLine();
+            System.out.println("Input password:");
+            password = sc.nextLine();
 
+            // TODO
+            // pass the username and password to the server
+            // in the server it will store these information with hashmap
+            // the hashmap can check if the user entered the correct password
 
-    Client(String ID, String username, String password) {
-        this.ID = ID;
-        this.username = username;
-        this.password = password;
-        this.status = false;
+        } else if (option == 2) { // Login
+            // TODO
+            // check the server if the account exist
+            // the server should have public hashmap which contain the username and password
 
-        // setting the socket
-        try {
-            socket = new Socket("127.0.0.1", 12345);
-            in = new DataInputStream(socket.getInputStream());
-            out = new DataOutputStream(socket.getOutputStream());
-        } catch (Exception e) {
-            System.out.println("The server is off");
-        }
+            // if it exist then login success
+            login_success = true;
+            // else fail then tell the user it is fail in the login process
+            
+            while (true) {
+                System.out.println("Input message and press ENTER");
+                String message = sc.nextLine();
 
-    }
+                int size = message.length();
+                out.writeInt(size);
+                out.write(message.getBytes(), 0, size);
 
-    private void change_password(String new_password) {
-        this.password = new_password;
-    }
-
-    public String get_id() {
-        return this.ID;
-    }
-
-    public String get_username() {
-        return this.username;
-    }
-
-    public String get_password() {
-        return this.password;
-    }
-
-    private void change_status(boolean s) {
-        this.status = s;
-    }
-
-    public void send_msg() {
-        System.out.println("Input message and press ENTER");
-        String message = scanner.nextLine();
-        try{
-            int size = message.length();
-            out.writeInt(size);
-            out.write(message.getBytes(), 0, size);
-
-            message = "";
-            size = in.readInt();
-            while(size > 0) {
-                int len = in.read(buffer, 0, Math.min(size, buffer.length));
-                message += new String(buffer, 0, len);
-                size -= len;
+                message = "";
+                size = in.readInt();
+                while (size > 0) {
+                    int len = in.read(buffer, 0, Math.min(size, buffer.length));
+                    message += new String(buffer, 0, len);
+                    size -= len;
+                }
+                System.out.println(message);
             }
-        }catch (Exception e){
-            System.out.println("error during sending msg");
         }
+
+    }
+
+    public static void main(String[] args) {
+        String serverIP = "127.0.0.1";
+        int port = 12345;
+        try {
+            new Client(serverIP, port);
+        } catch (Exception e) {
+            System.out.println("Init Client fail");
+        }
+
 
     }
 
--- src/Server.java
+++ src/Server.java
@@ -4,9 +4,12 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Server {
-    ArrayList<Socket> socketList = new ArrayList<Socket>();
+//    ArrayList<Socket> socketList = new ArrayList<Socket>();
+    HashMap<String, Socket> socketList = new HashMap<>();
+    private String id = "";
 
     public void print(String str, Object... o) {
         System.out.printf(str, o);
@@ -15,15 +18,16 @@ public class Server {
     public Server(int port) throws IOException {
         ServerSocket srvSocket = new ServerSocket(port);
 
-        while (true) {
+        while(true) {
             print("Listening at port %d...\n", port);
             Socket clientSocket = srvSocket.accept();
 
             synchronized (socketList) {
-                socketList.add(clientSocket);
+                // check if the user logged in the account, if yes then put it in to the socket list(hashmap)
+                socketList.put(id, clientSocket);
             }
 
-            Thread t = new Thread(() -> {
+            Thread t = new Thread(()-> {
                 try {
                     serve(clientSocket);
                 } catch (IOException ex) {
@@ -31,7 +35,7 @@ public class Server {
                 }
 
                 synchronized (socketList) {
-                    socketList.remove(clientSocket);
+                    socketList.remove(id);
                 }
             });
             t.start();
@@ -46,32 +50,33 @@ public class Server {
         DataInputStream in = new DataInputStream(clientSocket.getInputStream());
         DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
 
-        while (true) {
+        while(true) {
             int size = in.readInt();
-            String msg = "FORWARD: ";
-            while (size > 0) {
+            StringBuilder msg = new StringBuilder("FORWARD: ");
+            while(size > 0) {
                 int len = in.read(buffer, 0, Math.min(size, buffer.length));
-                msg += new String(buffer, 0, len);
+                msg.append(new String(buffer, 0, len));
                 size -= len;
             }
 
-            // forward(msg);
+            forward(msg.toString());
 
         }
     }
 
-    private void forward(String msg) {
+    private void forward(String msg){
         synchronized (socketList) {
-            for (Socket socket : socketList) {
-                try {
-                    DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-                    out.writeInt(msg.length());
-                    out.write(msg.getBytes(), 0, msg.length());
-                } catch (IOException ex) {
-                    print("Unable to forward message to %s:%d\n",
-                            socket.getInetAddress().getHostName(), socket.getPort());
-                }
-            }
+//            for (Socket socket : socketList) {
+//                try {
+//                    System.out.println("Socket: "+socket);
+//                    DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+//                    out.writeInt(msg.length());
+//                    out.write(msg.getBytes(), 0, msg.length());
+//                } catch (IOException ex) {
+//                    print("Unable to forward message to %s:%d\n",
+//                            socket.getInetAddress().getHostName(), socket.getPort());
+//                }
+//            }
         }
     }
 
