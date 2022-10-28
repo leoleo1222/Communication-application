b78ea2d833d82c44994bc3e6a12f9ea8c9198935
f1214638 <f1214638@csdlabad.local>
2022-10-28 10:09:19 +0800

    28/10 remove redundant code

--- src/Server.java
+++ src/Server.java
@@ -84,24 +84,6 @@ public class Server {
         }
     }
 
-    private StringBuilder read_header(Socket clientSocket){
-        byte[] buffer = new byte[1024];
-        StringBuilder msg = new StringBuilder("");
-        try {
-            DataInputStream in = new DataInputStream(clientSocket.getInputStream());
-            int size = in.readInt();
-            msg = new StringBuilder("");
-            while(size > 0) {
-                int len = in.read(buffer, 0, Math.min(size, buffer.length));
-                msg.append(new String(buffer, 0, len));
-                size -= len;
-            }
-        }catch (Exception e){
-            System.out.println("Fail in reading msg");
-        }
-        return msg;
-    }
-
     private StringBuilder read_txt(Socket clientSocket){
         byte[] buffer = new byte[1024];
         StringBuilder msg = new StringBuilder("");
