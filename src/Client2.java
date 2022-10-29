548c0dcb2c1b6b68d2def9090567db46f6fb524e
leoleo1222 <rx4498@gmail.com>
2022-10-29 21:24:45 +0800

    29/10 add new class for testing, client2 is same as client

---
+++ src/Client2.java
@@ -0,0 +1,86 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client2 {
+    public static String username; // the username of the user
+    private static String password; // the password of the user
+    public boolean status; // the online/offline status of the user
+    private static boolean login_success = false;
+    byte[] buffer = new byte[1024];
+
+    public Client2(String serverIP, int port) throws IOException {
+        System.out.printf("Connecting to %s:%d\n", serverIP, port);
+        Socket socket = new Socket(serverIP, port);
+        DataInputStream in = new DataInputStream(socket.getInputStream());
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        byte[] buffer = new byte[1024];
+        Scanner sc = new Scanner(System.in);
+        while (true){
+            System.out.println("1)Registration\n2)Login");
+            int option = sc.nextInt(); // choose register or login
+            sc.nextLine();
+            if (option == 1) { // Registration
+                System.out.println("=== Registration ===");
+                System.out.println("Input username:");
+                username = sc.nextLine();
+                System.out.println("Input password:");
+                password = sc.nextLine();
+                String header = "reg";
+                int header_size = header.length();
+                out.writeInt(header_size);
+                out.write(header.getBytes(), 0, header_size);
+                int username_size = username.length();
+                out.writeInt(username_size);
+                out.write(username.getBytes(), 0, username_size);
+                int password_size = password.length();
+                out.writeInt(password_size);
+                out.write(password.getBytes(), 0, password_size);
+            } else if (option == 2) { // Login
+                // TODO
+                // check the server if the account exist
+                // the server should have public hashmap which contain the username and password
+
+                while (true) {
+                    System.out.println("Receiver:");
+                    String header = sc.nextLine();
+                    int header_size = header.length();
+                    out.writeInt(header_size);
+                    out.write(header.getBytes(), 0, header_size);
+
+                    System.out.println("Input message and press ENTER");
+                    String message = sc.nextLine();
+
+                    int size = message.length();
+                    out.writeInt(size);
+                    out.write(message.getBytes(), 0, size);
+                    
+                    // message
+                    message = "";
+                    size = in.readInt();
+                    while(size > 0) {
+                        int len = in.read(buffer, 0, Math.min(size, buffer.length));
+                        message += new String(buffer, 0, len);
+                        size -= len;
+                    }
+
+                    System.out.println(message);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String serverIP = "127.0.0.1";
+        int port = 12345;
+        try {
+            new Client2(serverIP, port);
+        } catch (Exception e) {
+            System.out.println("Init Client fail");
+        }
+    }
+
+
+}
